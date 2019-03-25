2cdbe88e7b36d1d13378786351538720cc3ee5e6
buuug7 <youpp@126.com>
2019-03-25 14:27:57 +0800

    update more

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -3,7 +3,7 @@ import React from 'react';
 function HelloWorld () {
   return (
     <div className='HelloWorld'>
-      <h4>Create React App Lite</h4>
+      <h4 style={{ textAlign: 'center' }}>Create React App Lite</h4>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipisicing elit. A adipisci asperiores distinctio enim esse fuga illo
         inventore ipsam ipsum iste modi mollitia natus necessitatibus odio, officia quisquam reprehenderit rerum
