e06cd334374f52666288611bfac7a63543c57b49
buuug7 <puguohong@gmail.com>
2019-07-07 14:11:45 +0800

    update component of HelloWorld.js

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,23 +1,22 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react'
 
 function HelloWorld () {
 
   const [data, setData] = useState({
     name: '',
-    description: ''
-  });
+    description: '',
+  })
 
   useEffect(() => {
-    fetch('/data.json').then(res => res.json())
-    .then(json => setData(json))
-  } ,[])
+    fetch('/data.json').then(res => res.json()).then(json => setData(json))
+  }, [])
 
   return (
     <div className='HelloWorld'>
       <h4 style={{ textAlign: 'center' }}>{data.name}</h4>
       <p>{data.description}</p>
     </div>
-  );
+  )
 }
 
-export default HelloWorld;
+export default HelloWorld
