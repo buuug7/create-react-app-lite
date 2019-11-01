034c5f07f78f5e0911dd0b8458f10b682fb0ddcf
buuug7 <youpp@126.com>
2019-11-02 01:00:51 +0800

    update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,8 @@ module.exports = {
     )
   ],
   devServer: {
-    contentBase: [path.join(__dirname, 'dist')]
+    // contentBase: [path.join(__dirname, 'static')],
+    publicPath: PUBLIC_PATH
   },
   module: {
     rules: [
