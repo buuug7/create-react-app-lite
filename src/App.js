2e8711708c93dc84a96020b946351a43296da1cb
buuug7 <youpp@126.com>
2024-07-18 09:57:55 +0800

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
-import React from 'react';
-import HelloWorld from './components/HelloWorld';
+import React from "react";
+import HelloWorld from "./components/HelloWorld";
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <HelloWorld />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <HelloWorld />
+    </div>
+  );
 }
 
 export default App;
